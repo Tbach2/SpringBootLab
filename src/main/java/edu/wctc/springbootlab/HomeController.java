e7f859ddc09d75998b8329f326e687cbcadacbac
Tyler Bach <Tbach2@my.wctc.edu>
2022-02-10 22:39:53 -0800

    Initial commit

---
+++ src/main/java/edu/wctc/springbootlab/HomeController.java
@@ -0,0 +1,36 @@
+package edu.wctc.springbootlab;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/cart")
+    public String showCartPage() {
+        return "orders/cart";
+    }
+
+    @RequestMapping("/product-detail")
+    public String showProductDetailPage() {
+        return "catalog/product-detail";
+    }
+
+    @PostMapping("/place-order")
+    public String showPlaceOrder() {
+        return "index";
+    }
+
+    @RequestMapping("/search")
+    public String redirectToGoogle() {
+        return "redirect:https://www.google.com";
+    }
+}
+
